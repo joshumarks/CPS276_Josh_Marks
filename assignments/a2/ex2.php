e8462ca4ff1eae192a373259269d073bbee6e788
Josh Marks <jtmarks@wccnet.edu>
2024-09-26 10:06:56 -0400

    Finished example 2

--- assignments/a2/ex2.php
+++ assignments/a2/ex2.php
@@ -1,16 +1,54 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<?php
 
-    <title>Name List</title>
-  </head>
-  <body>
-    <main class="container">
-        <h1>Test</h1>
-    </main>
-  </body>
+$title = "My Web Page";
+$heading = "My Web page";
+$name = "Josh Marks";
+$footer = "My Web Page ©2024";
+
+$pgraph_text = "";
+$latin = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat mollis dolor at bibendum. In congue maximus ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet bibendum erat, sed bibendum eros cursus eu. Nulla at neque rhoncus, ultricies odio at, accumsan elit. Proin in turpis eu leo dapibus pulvinar. Vivamus viverra massa ut enim fringilla ultricies. Donec in enim blandit, iaculis nulla quis, egestas elit. Nullam ut enim id erat bibendum finibus nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet egestas.";
+
+
+for($i=0; $i < 3; ++$i)
+{ 
+  $pgraph_text .= "<p>{$latin}</p>";
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> <?php echo $title; ?> </title>
+	<style>
+		* {margin: 0; padding: 0;}
+		body {font: 120%/1.5 sans-serif;}
+		#wrapper {width: 1000px; margin: 0 auto; border: 1px solid black;}
+		header {background: green; height: 150px; padding: 20px;}
+		header h1 {color: white;}
+		main {padding: 10px;}
+		main h2 {margin: 15px 0;}
+		main p {margin-bottom: 15px;}
+		footer {background: #eee; padding: 10px 0; text-align: center}
+		footer p {font-size: .8em;}
+	</style>
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<h1> <?php echo $heading; ?> </h1>
+		</header>
+		<main>
+			<h2>My name is <?php echo $name; ?> </h2>
+			<p> <?php echo $pgraph_text; ?> </p>
+		</main>
+		<footer>
+			<p> <?php echo $footer; ?> </p>
+		</footer>
+	</div>
+	
+</body>
 </html>

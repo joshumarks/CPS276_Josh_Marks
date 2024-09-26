e94a5f49858a3af9f3bca610d6231ccf9928eaa0
Josh Marks <jtmarks@wccnet.edu>
2024-09-26 10:09:14 -0400

    Touched up exercise 2

--- assignments/a2/ex2.php
+++ assignments/a2/ex2.php
@@ -8,13 +8,10 @@ $footer = "My Web Page ©2024";
 $pgraph_text = "";
 $latin = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat mollis dolor at bibendum. In congue maximus ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet bibendum erat, sed bibendum eros cursus eu. Nulla at neque rhoncus, ultricies odio at, accumsan elit. Proin in turpis eu leo dapibus pulvinar. Vivamus viverra massa ut enim fringilla ultricies. Donec in enim blandit, iaculis nulla quis, egestas elit. Nullam ut enim id erat bibendum finibus nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet egestas.";
 
-
 for($i=0; $i < 3; ++$i)
 { 
   $pgraph_text .= "<p>{$latin}</p>";
 }
-
-
 
 ?>
 
